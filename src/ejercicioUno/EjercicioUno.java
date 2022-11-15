3cf87801e4c18381a909576202b45d102b95d8ac
fabriconiglio <fconiglio100@gmail.com>
2022-11-15 19:24:58 -0300

    subo ejercicio 1

---
+++ src/ejercicioUno/EjercicioUno.java
@@ -0,0 +1,19 @@
+package ejercicioUno;
+
+
+public class EjercicioUno {
+
+    public static void main(String[] args) {
+
+        //Tipos de datos
+        int entero = 10;
+        double decimal = 10.5;
+        char caracter = 'a';
+        String cadena = "Hola";
+        boolean booleano = true;
+        long enteroLargo = 1000000000000000000L;
+
+        //Imprimir en consola
+        System.out.println("Entero: " + entero + " ,Decimal: " + decimal + " ,Caracter: " + caracter + " ,Cadena: " + cadena + " ,Booleano: " + booleano + " ,Entero largo: " + enteroLargo);
+    }
+}
